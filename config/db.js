590e4bcee3b78fa1ce42b3c918257eb9d06c76a6
daniyal husain <daniyalhusain10@gmail.com>
2025-12-30 15:30:07 +0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,8 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      connectTimeoutMS: 60000,      // 60 seconds wait
-      serverSelectionTimeoutMS: 60000, // server select hone tak wait
+      connectTimeoutMS: 60000,       // 60 seconds wait
+      serverSelectionTimeoutMS: 60000 // server select hone tak wait
     });
     console.log("✅ MongoDB connected");
   } catch (error) {
